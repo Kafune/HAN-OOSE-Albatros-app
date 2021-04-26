055b5c6b2870c735c5ee952c233fb0ebf7b092ee
Kachung Li <KC.Li@student.han.nl>
2021-04-26 16:27:52 +0200

    point of interests overzicht

--- src/pages/NewRoutePage.tsx
+++ src/pages/NewRoutePage.tsx
@@ -1,4 +1,4 @@
-import React, { FC, Fragment, useState } from 'react';
+import React, { FC, Fragment, useEffect, useState } from 'react';
 import { View, Button, StyleSheet, Text, ScrollView, Pressable } from 'react-native';
 import Maps from '../components/Maps';
 import { Coordinate } from '../core/domain/Coordinate';
@@ -17,11 +17,14 @@ const NewRoutesPage: FC = () => {
     const [selectCor, setSelectCor] = useState<boolean>(false);
     const [centerPoint, setCenterPoint] = useState([5.6679899, 52.0430533]);
     const [mapPoints, setMapPoints] = useState([[5.6679899, 52.0430533], [5.664037711124264, 52.04006754102576]]);
+    const [POIArray, setPOIArray] = useState([[1, "Kerk", "Een mooie middeleeuwse kerk. Dit is een lange beschrijving van een point of interest."]]);
     const [POIDialog, setPOIDialog] = useState(false);
+    const [currentMapPoint, setCurrentMapPoint] = useState([]);
+    const [currentPOIName, setCurrentPOIName] = useState("");
+    const [currentPOIDescription, setCurrentPOIDescription] = useState("");
 
     function addCoordinate(coordinate: Coordinate) {
         setSelectCor(false);
-        console.log(coordinate);
     }
 
     const mapCoordinateMapper = () => {
@@ -30,23 +33,23 @@ const NewRoutesPage: FC = () => {
         )
     }
 
-    const createNewPOI = () => {
-        return <View>
-            <Dialog.Container visible={POIDialog}>
-                <Dialog.Title>Set a new POI</Dialog.Title>
-                <Dialog.Button label="Cancel" onPress={handleCancel}></Dialog.Button>
-                <Dialog.Button label="Create" onPress={handleCreatePOI}></Dialog.Button>
-
-            </Dialog.Container>
-        </View>
+    const showPOIDialog = (mapPoint) => {
+        setCurrentMapPoint(mapPoint);
+        setPOIDialog(true);
     }
 
     const handleCancel = () => {
-
+        setCurrentMapPoint([]);
+        setPOIDialog(false);
     }
 
-    const handleCreatePOI = () => {
-
+    const handleCreatePOI = (name: string, description: string) => {
+        //TODO: zorg ervoor dat de POI in de correcte segment komt.
+        setPOIArray([...POIArray, [mapPoints.findIndex(currentPoint => currentPoint == currentMapPoint) + 1, name, description]])
+        setCurrentMapPoint([]);
+        setCurrentPOIName("");
+        setCurrentPOIDescription("");
+        setPOIDialog(false);
     }
 
 
@@ -76,51 +79,77 @@ const NewRoutesPage: FC = () => {
                 />
 
             </View>
-            {mapPoints.map(mapPoint => {
-                return <Pressable style={styles.wrapper} key={JSON.stringify(mapPoint)} onPress={() => {
-                    setCenterPoint([
-                        mapPoint[0], mapPoint[1]
-                    ])
-                    console.log(centerPoint)
-
-                }}>
-                    <MaterialCommunityIcons
-                        name="map-marker"
-                        size={30}
-                        color={colors.main}
-                    />
-                    <View style={styles.segmentData}>
-                        <Text>Latitude: {mapPoint[1]}{"\n"}</Text>
-                        <Text>Longtitude: {mapPoint[0]}{"\n"}</Text>
-                    </View>
-                    <Pressable style={styles.POIButton} onPress={() => setPOIDialog(true)}>
-                        <View>
-                            <Text>Add POI</Text>
-                        </View>
-                    </Pressable>
-                    <View style={styles.button}>
+            {mapPoints.map((mapPoint, index) => {
+                return <Fragment key={JSON.stringify(mapPoint)}>
+                    <Pressable style={styles.wrapper} onPress={() => {
+                        setCenterPoint([
+                            mapPoint[0], mapPoint[1]
+                        ])
+                    }}>
                         <MaterialCommunityIcons
-                            name="close-outline"
+                            name="map-marker"
                             size={30}
                             color={colors.main}
-                            onPress={() => {
-
-                            }}
                         />
-                    </View>
-                </Pressable>
+                        <View style={styles.segmentData}>
+                            <Text>Latitude: {mapPoint[1]}{"\n"}</Text>
+                            <Text>Longtitude: {mapPoint[0]}{"\n"}</Text>
+                        </View>
+                        <Pressable style={styles.POIButton} onPress={() => showPOIDialog(mapPoint)}>
+                            <View>
+                                <Text>Add POI</Text>
+                            </View>
+                        </Pressable>
+                        <View style={styles.button}>
+                            <MaterialCommunityIcons
+                                name="close-outline"
+                                size={30}
+                                color={colors.main}
+                                onPress={() => {
+
+                                }}
+                            />
+                        </View>
+                    </Pressable>
+                    {POIArray.map((POI) => {
+                        if (POI[0] == index + 1) {
+                            return <View key={JSON.stringify(POI[0])} style={[styles.wrapper, styles.POIWrapper]}>
+                                <View style={styles.segmentData}>
+                                    <Text>POI nr: {POI[0]}</Text>
+                                    <Text>POI name: {POI[1]}</Text>
+                                    <Text>POI Description: {POI[2]}</Text>
+                                    <View style={styles.POI_Icons}>
+                                        <MaterialCommunityIcons
+                                            name="pencil"
+                                            size={30}
+                                            color={colors.main}
+                                            onPress={() => {
+
+                                            }}
+                                        />
+                                        <MaterialCommunityIcons
+                                            name="close-outline"
+                                            size={30}
+                                            color={colors.main}
+                                            onPress={() => {
+
+                                            }}
+                                        />
+                                    </View>
+                                </View>
+                            </View>
+                        }
+                    })}
+                </Fragment>
             })}
-            <View>
-                <Text>{geoData[0].segments[0].poi.name}</Text>
-                <Text>{geoData[0].segments[0].poi.description}</Text>
-            </View>
+
             <View>
                 <Dialog.Container visible={POIDialog}>
                     <Dialog.Title>Set a new POI</Dialog.Title>
-                    <Dialog.Input label="Name"></Dialog.Input>
-                    <Dialog.Input label="Description"></Dialog.Input>
-                    <Dialog.Button label="Cancel" onPress={handleCancel}></Dialog.Button>
-                    <Dialog.Button label="Create" onPress={handleCreatePOI}></Dialog.Button>
+                    <Dialog.Input label="Name" onChangeText={name => setCurrentPOIName(name)}></Dialog.Input>
+                    <Dialog.Input label="Description" onChangeText={description => setCurrentPOIDescription(description)}></Dialog.Input>
+                    <Dialog.Button label="Cancel" onPress={() => handleCancel()}></Dialog.Button>
+                    <Dialog.Button label="Create" onPress={() => handleCreatePOI(currentPOIName, currentPOIDescription)}></Dialog.Button>
                 </Dialog.Container>
             </View>
 
@@ -145,11 +174,13 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'row',
         backgroundColor: '#FFFFFF',
-        marginBottom: 10,
         marginHorizontal: 10,
         padding: 5,
         overflow: 'hidden',
         borderRadius: 6,
+    },
+    POIWrapper: {
+        marginBottom: 10,
     },
     segmentNumber: {
         borderWidth: 1,
@@ -162,6 +193,9 @@ const styles = StyleSheet.create({
     segmentData: {
         flex: 3,
         flexDirection: "column"
+    },
+    POI_Icons: {
+        flexDirection: "row"
     },
     POIButton: {
         backgroundColor: colors.main
