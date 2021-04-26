14c743c9257e95550e8a37be814d54b3f33fce5b
Kachung Li <KC.Li@student.han.nl>
2021-04-26 11:59:11 +0200

    mappoints in map weergeven en start POI dialog

--- src/pages/NewRoutePage.tsx
+++ src/pages/NewRoutePage.tsx
@@ -1,131 +1,137 @@
 import React, { FC, Fragment, useState } from 'react';
 import { View, Button, StyleSheet, Text, ScrollView, Pressable } from 'react-native';
 import Maps from '../components/Maps';
-import { Route } from '../core/domain/Route';
-import { Segment } from '../core/domain/Segment';
+import { Coordinate } from '../core/domain/Coordinate';
+import SelectCoordinate from '../components/SelectCoordinate';
 import { MapsLine } from '../core/maps/MapsLine';
 import { MapsCoordinate } from '../core/maps/MapsCoordinate';
 import { MapsPoint } from '../core/maps/MapsPoints';
 import GeoData from '../api/GeoData';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import colors from '../styles/colors';
+import Dialog from 'react-native-dialog';
 
 const NewRoutesPage: FC = () => {
     const geoData = GeoData.getGeoData();
 
-    const [currentStart, setCurrentStart] = useState({ latitude: 52.04006754102576, longitude: 5.664037711124264 });
-    const [currentEnd, setCurrentEnd] = useState();
+    const [selectCor, setSelectCor] = useState<boolean>(false);
+    const [centerPoint, setCenterPoint] = useState([5.6679899, 52.0430533]);
+    const [mapPoints, setMapPoints] = useState([[5.6679899, 52.0430533], [5.664037711124264, 52.04006754102576]]);
+    const [POIDialog, setPOIDialog] = useState(false);
 
-    const addSegment = (segment: Segment) => {
+    function addCoordinate(coordinate: Coordinate) {
+        setSelectCor(false);
+        console.log(coordinate);
+    }
+
+    const mapCoordinateMapper = () => {
+        return mapPoints.map(mapPoint =>
+            new MapsCoordinate(mapPoint[0], mapPoint[1])
+        )
+    }
+
+    const createNewPOI = () => {
+        return <View>
+            <Dialog.Container visible={POIDialog}>
+                <Dialog.Title>Set a new POI</Dialog.Title>
+                <Dialog.Button label="Cancel" onPress={handleCancel}></Dialog.Button>
+                <Dialog.Button label="Create" onPress={handleCreatePOI}></Dialog.Button>
+
+            </Dialog.Container>
+        </View>
+    }
+
+    const handleCancel = () => {
 
     }
 
-    // const addCordinate: Coordinate = (coordinate: Coordinate) => {
-    //     setSelectCor(true);
-    // }
+    const handleCreatePOI = () => {
 
-    return <ScrollView>
-        <View>
-            <Text>{JSON.stringify(currentStart)}</Text>
+    }
 
-            <Maps
-                mapsLine={
-                    new MapsLine([
-                        new MapsCoordinate(geoData[0].segments[0].start.longitude, geoData[0].segments[0].start.latitude),
-                        new MapsCoordinate(geoData[0].segments[0].end.longitude, geoData[0].segments[0].end.latitude)
-                    ])
-                }
-                mapsPoint={
-                    new MapsPoint([
-                        new MapsCoordinate(geoData[0].segments[0].start.longitude, geoData[0].segments[0].start.latitude),
-                        new MapsCoordinate(geoData[0].segments[0].end.longitude, geoData[0].segments[0].end.latitude),
-                    ])
-                }
-                center={[currentStart.longitude, currentStart.latitude]}
-                zoom={13}
-            />
 
-        </View>
-        <Pressable style={styles.wrapper} onPress={() => setCurrentStart({
-            latitude: geoData[0].segments[0].start.latitude,
-            longitude: geoData[0].segments[0].start.longitude
-        })}>
-            <Text style={styles.segmentNumber}>
-                {JSON.stringify(geoData[0].segments[0].id)}
-            </Text>
-            <Text>Startpunt: </Text>
-            <View style={styles.segmentData}>
-                <Text>Latitude: {JSON.stringify(geoData[0].segments[0].start.latitude)}{"\n"}</Text>
-                <Text>Longtitude: {JSON.stringify(geoData[0].segments[0].start.longitude)}{"\n"}</Text>
-                <Text>Altitude: {JSON.stringify(geoData[0].segments[0].start.altitude)}{"\n"}</Text>
+    if (selectCor) {
+        return (
+            <View></View>
+            // <SelectCoordinate
+            //     addCoordinate={addCoordinate}
+            //     cancel={() => {
+            //         setSelectCor(false);
+            //     }}
+            // />
+        );
+    } else {
+        return <ScrollView>
+            <View>
+
+                <Maps
+                    mapsLine={
+                        new MapsLine(mapCoordinateMapper())
+                    }
+                    mapsPoint={
+                        new MapsPoint(mapCoordinateMapper())
+                    }
+                    center={centerPoint}
+                    zoom={13}
+                />
+
             </View>
-            <Text>Eindpunt: </Text>
-            <View style={styles.segmentData}>
-                <Text>Latitude: {JSON.stringify(geoData[0].segments[0].end.latitude)}{"\n"}</Text>
-                <Text>Longtitude: {JSON.stringify(geoData[0].segments[0].end.longitude)}{"\n"}</Text>
-                <Text>Altitude: {JSON.stringify(geoData[0].segments[0].end.altitude)}{"\n"}</Text>
+            {mapPoints.map(mapPoint => {
+                return <Pressable style={styles.wrapper} key={JSON.stringify(mapPoint)} onPress={() => {
+                    setCenterPoint([
+                        mapPoint[0], mapPoint[1]
+                    ])
+                    console.log(centerPoint)
+
+                }}>
+                    <MaterialCommunityIcons
+                        name="map-marker"
+                        size={30}
+                        color={colors.main}
+                    />
+                    <View style={styles.segmentData}>
+                        <Text>Latitude: {mapPoint[1]}{"\n"}</Text>
+                        <Text>Longtitude: {mapPoint[0]}{"\n"}</Text>
+                    </View>
+                    <Pressable style={styles.POIButton} onPress={() => setPOIDialog(true)}>
+                        <View>
+                            <Text>Add POI</Text>
+                        </View>
+                    </Pressable>
+                    <View style={styles.button}>
+                        <MaterialCommunityIcons
+                            name="close-outline"
+                            size={30}
+                            color={colors.main}
+                            onPress={() => {
+
+                            }}
+                        />
+                    </View>
+                </Pressable>
+            })}
+            <View>
+                <Text>{geoData[0].segments[0].poi.name}</Text>
+                <Text>{geoData[0].segments[0].poi.description}</Text>
+            </View>
+            <View>
+                <Dialog.Container visible={POIDialog}>
+                    <Dialog.Title>Set a new POI</Dialog.Title>
+                    <Dialog.Input label="Name"></Dialog.Input>
+                    <Dialog.Input label="Description"></Dialog.Input>
+                    <Dialog.Button label="Cancel" onPress={handleCancel}></Dialog.Button>
+                    <Dialog.Button label="Create" onPress={handleCreatePOI}></Dialog.Button>
+                </Dialog.Container>
+            </View>
+
+            <View style={styles.button}>
+                <Button title="Een punt toevoegen" onPress={() => { setSelectCor(true) }} />
             </View>
             <View style={styles.button}>
-                <MaterialCommunityIcons
-                    name="close-outline"
-                    size={30}
-                    color={"#000000"}
-                    onPress={() => {
-
-                    }}
-                />
+                <Button title="Route opslaan" onPress={() => { }} />
             </View>
-        </Pressable>
-        <View>
-            <Text>{geoData[0].segments[0].poi.name}</Text>
-            <Text>{geoData[0].segments[0].poi.description}</Text>
-        </View>
-        {/* {geoData.map((route: Route) => {  
-                route.segments.map((segment: Segment) => {
-                    <>
-                        <Pressable style={styles.wrapper} onPress={() => setCurrentStart({
-                            latitude: segment.start.latitude,
-                            longitude: segment.start.longitude
-                        })}>
-                            <Text style={styles.segmentNumber}>
-                                {JSON.stringify(segment.id)}
-                            </Text>
-                            <Text>Startpunt: </Text>
-                            <View style={styles.segmentData}>
-                                <Text>Latitude: {JSON.stringify(segment.start.latitude)}{"\n"}</Text>
-                                <Text>Longtitude: {JSON.stringify(segment.start.longitude)}{"\n"}</Text>
-                                <Text>Altitude: {JSON.stringify(segment.start.altitude)}{"\n"}</Text>
-                            </View>
-                            <Text>Eindpunt: </Text>
-                            <View style={styles.segmentData}>
-                                <Text>Latitude: {JSON.stringify(segment.end.latitude)}{"\n"}</Text>
-                                <Text>Longtitude: {JSON.stringify(segment.end.longitude)}{"\n"}</Text>
-                                <Text>Altitude: {JSON.stringify(segment.end.altitude)}{"\n"}</Text>
-                            </View>
-                            <View style={styles.button}>
-                                <MaterialCommunityIcons
-                                    name="close-outline"
-                                    size={30}
-                                    color={"#000000"}
-                                    onPress={() => {
-
-                                    }}
-                                />
-                            </View>
-                        </Pressable>
-                        <View>
-                            <Text>{segment.poi.name}</Text>
-                            <Text>{segment.poi.description}</Text>
-                        </View>
-                    </>
-                });
-            })} */}
-        <View style={styles.button}>
-            <Button title="Een punt toevoegen" onPress={() => { }} />
-        </View>
-        <View style={styles.button}>
-            <Button title="Route opslaan" onPress={() => addSegment} />
-        </View>
-    </ScrollView>
+        </ScrollView>
+    }
 }
 
 const styles = StyleSheet.create({
@@ -141,7 +147,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#FFFFFF',
         marginBottom: 10,
         marginHorizontal: 10,
-        padding: 15,
+        padding: 5,
         overflow: 'hidden',
         borderRadius: 6,
     },
@@ -156,6 +162,9 @@ const styles = StyleSheet.create({
     segmentData: {
         flex: 3,
         flexDirection: "column"
+    },
+    POIButton: {
+        backgroundColor: colors.main
     }
 })
 
