0bba2d45447aaff106f3c87fe4ef10534217e088
Robert Boudewijn <RP.boudewijn@student.han.nl>
2021-05-11 15:07:07 +0200

    finished

--- src/pages/RecordActivity.tsx
+++ src/pages/RecordActivity.tsx
@@ -1,13 +1,16 @@
 import React, {FC, useState, useEffect} from 'react';
+import {Button} from 'react-native';
 import TrackMap from '../components/TrackMap';
 import {MapsLine} from '../core/maps/MapsLine';
 import {MapsCoordinate} from '../core/maps/MapsCoordinate';
 import {MapsPoint} from '../core/maps/MapsPoints';
 import getLocation from '../core/maps/GetLocation';
+import {Error} from '../components/Error';
 
 const RecordActivity: FC = () => {
-  const GPS_INTERVAL = 2000; // get GPS every 2000ms
-  const [paused, setPaused] = useState<boolean>(false);
+  const GPS_INTERVAL = 2500; // get GPS every 2500ms
+  const [intervalNr, setIntervalNr] = useState<Number>(0);
+  const [tracking, setTracking] = useState<boolean>(true);
   const [route, setRoute] = useState<MapsLine>(
     new MapsLine([
       new MapsCoordinate(5.674306, 52.030944),
@@ -20,7 +23,8 @@ const RecordActivity: FC = () => {
   const [walkedRoute, setWalkedRoute] = useState<MapsLine>(
     new MapsLine([new MapsCoordinate(5.674306, 52.030954)]),
   );
-  //Super ugly fix. After trying for 8 hours,t TrackMap woudnt update. So I did this. Ill look at it tomorrow
+  //Trackmap is in a state because it wouldn't update from its own.
+  //Using a state makes sure it will allways be updated.
   const [Map, setMap] = useState<object>(
     <TrackMap
       mapsLines={[route, walkedRoute]}
@@ -31,37 +35,55 @@ const RecordActivity: FC = () => {
   );
 
   useEffect(() => {
-    const excecuteFunction = async () => {
-      const COORDINATE = await getLocation();
-      if (!COORDINATE?.longitude || !COORDINATE?.latitude) {
-        //error handle|
+    const trackGPS = async () => {
+      try {
+        const COORDINATE = await getLocation();
+        setTracking(true);
+        const MAPLINE = walkedRoute;
+        MAPLINE.pushCoordinate(
+          new MapsCoordinate(COORDINATE.longitude, COORDINATE.latitude),
+        );
+
+        setWalkedRoute(MAPLINE);
+        setMap(
+          <TrackMap
+            mapsLines={[route, MAPLINE]}
+            mapsPoint={new MapsPoint([])}
+            center={walkedRoute.getLastCoordinate().toArray()}
+            zoom={14}
+          />,
+        );
+        console.log(walkedRoute);
+      } catch {
+        setTracking(false);
         return;
       }
-      const MAPLINE = walkedRoute;
-      MAPLINE.pushCoordinate(
-        new MapsCoordinate(COORDINATE.longitude, COORDINATE.latitude),
-      );
-
-      setWalkedRoute(MAPLINE);
-      setMap(
-        <TrackMap
-          mapsLines={[route, MAPLINE]}
-          mapsPoint={new MapsPoint([])}
-          center={walkedRoute.getLastCoordinate().toArray()}
-          zoom={14}
-        />,
-      );
-      console.log(walkedRoute);
     };
 
-    const interval = setInterval(async () => {
-      excecuteFunction();
-    }, GPS_INTERVAL);
+    setIntervalNr(
+      //@ts-ignore - In normal js (also here) setInterval returns a number.
+      //ESLint thinks that it isnt a number but a Datatype that doesn't excists.
+      setInterval(() => {
+        trackGPS();
+      }, GPS_INTERVAL),
+    );
+  }, [intervalNr, route, walkedRoute]);
+  console.log(intervalNr);
 
-    return () => clearInterval(interval);
-  }, [walkedRoute]);
-
-  return <>{Map}</>;
+  return (
+    <>
+      {tracking ? <></> : <Error errorCode={500} message={undefined} />}
+      {Map}
+      <Button
+        onPress={() => {
+          //@ts-ignore - In normal js (also here) setInterval returns a number.
+          //ESLint thinks that it isnt a number but a Datatype that doesn't excists.
+          clearInterval(intervalNr);
+        }}
+        title="stoppen"
+      />
+    </>
+  );
 };
 
 export default RecordActivity;
