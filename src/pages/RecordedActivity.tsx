229af59bc78e5b48163ac7b2e6817ebad13904a6
Patrick Roelofs <PJL.Roelofs@student.han.nl>
2021-05-20 10:39:45 +0200

    update styling

--- src/pages/RecordedActivity.tsx
+++ src/pages/RecordedActivity.tsx
@@ -1,9 +1,11 @@
 import React, {FC, useEffect} from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, Pressable} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {useSelector} from 'react-redux';
 import Maps from '../components/Maps';
+import colors, {brittishPalette} from '../styles/colors';
 import {RouteMapper} from '../core/mapper/RouteMapper';
-import { MapsPoint } from '../core/maps/MapsPoints';
+import {MapsPoint} from '../core/maps/MapsPoints';
 
 const RecordedActivity: FC = () => {
   const recordedActivityState = useSelector(state => state.walkedRoute);
@@ -25,6 +27,64 @@ const RecordedActivity: FC = () => {
           center={recordedActivityState.middlePoint}
         />
       </View>
+
+      <View style={styles.activityGrid}>
+        <View style={styles.activityItem}>
+          <View>
+            <Text style={styles.activityHeaderText}>Route Naam:</Text>
+            <Text style={styles.activityItemText}>...</Text>
+          </View>
+          <View>
+            <Text style={styles.activityHeaderText}>Afstand:</Text>
+            <Text style={styles.activityItemText}>...</Text>
+          </View>
+        </View>
+
+        <View style={styles.activityItem}>
+          <View>
+            <Text style={styles.activityHeaderText}>Gelopen Tijd:</Text>
+            <Text style={styles.activityItemText}>...</Text>
+          </View>
+          <View>
+            <Text style={styles.activityHeaderText}>Tempo:</Text>
+            <Text style={styles.activityItemText}>...</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.activityGrid}>
+        <View style={{...styles.activityItem, ...styles.activityButtonWrapper}}>
+          <View style={styles.button}>
+            <Pressable
+              onPress={() => {
+                console.log('pressed');
+              }}>
+              <MaterialCommunityIcons
+                name="delete"
+                size={30}
+                color={brittishPalette.white}
+              />
+              <Text style={styles.buttonText}>Verwijderen</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style={{...styles.activityItem, ...styles.activityButtonWrapper}}>
+          <View style={styles.button}>
+            <Pressable
+              onPress={() => {
+                console.log('pressed');
+              }}>
+              <MaterialCommunityIcons
+                name="content-save"
+                size={30}
+                color={brittishPalette.white}
+              />
+              <Text style={styles.buttonText}>Opslaan</Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+
       <View />
     </View>
   );
@@ -45,6 +105,46 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     paddingVertical: 12,
+  },
+  activityGrid: {
+    marginTop: 32,
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+  },
+  activityItem: {
+    width: '50%',
+  },
+  activityHeaderText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    textAlign: 'center',
+  },
+  activityItemText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 32,
+  },
+  activityButtonWrapper: {
+    textAlign: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: colors.main,
+    borderRadius: 5,
+    padding: 8,
+    marginTop: 25,
+    width: 150,
+  },
+  buttonText: {
+    paddingTop: 2,
+    fontSize: 18,
+    color: brittishPalette.white,
+    fontWeight: 'bold',
+    marginLeft: 5,
   },
 });
 
