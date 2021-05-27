fd123b0385e03356a5d063676df84c4d3e04d4e6
Robert Boudewijn <RP.boudewijn@student.han.nl>
2021-05-27 16:44:08 +0200

    Updated to work with tokens

--- src/core/controller/ActivityController.ts
+++ src/core/controller/ActivityController.ts
@@ -2,7 +2,10 @@ import {ActivityDTO} from '../dto/ActivityDTO';
 import api from '../data/api';
 
 export class ActivityController {
-  static async post(activity: ActivityDTO) {
-    await fetch(`${api.baseUrl}/activities`, api.headersPost(activity));
+  static async post(activity: ActivityDTO, token: number) {
+    await fetch(
+      `${api.baseUrl}/activities?token=${token}`,
+      api.headersPost(activity),
+    );
   }
 }
