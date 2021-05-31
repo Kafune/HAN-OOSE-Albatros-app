c09d9b6bce29a4908364118b312eb475aa719378
Patrick Roelofs <PJL.Roelofs@student.han.nl>
2021-05-31 11:20:20 +0200

    add FeedPage.tsx

---
+++ src/pages/FeedPage.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {useSelector} from 'react-redux';
+
+const FeedPage: FC = () => {
+
+  return (
+  );
+};
+export default FeedPage;
