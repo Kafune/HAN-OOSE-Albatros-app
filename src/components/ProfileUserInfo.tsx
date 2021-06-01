942e6bda72a90a651b0117505a76a01d458fe933
Kachung Li <KC.Li@student.han.nl>
2021-06-01 15:00:57 +0200

    speed calc moved to function, styling profile page progress

--- src/components/ProfileUserInfo.tsx
+++ src/components/ProfileUserInfo.tsx
@@ -1,25 +1,46 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {brittishPalette} from '../styles/colors';
 
 interface Props {
   label: String;
   value: any;
+  icon: string;
 }
 
-const ProfileUserInfo: React.FC<Props> = ({label, value}): JSX.Element => {
+const ProfileUserInfo: React.FC<Props> = ({
+  label,
+  value,
+  icon,
+}): JSX.Element => {
+  const size = 24;
   return (
     <View style={styles.wrapper}>
+      <MaterialCommunityIcons
+        name={icon}
+        size={size}
+        color={colors.iconColor}
+        style={styles.icon}
+      />
       <Text>{label}</Text>
       <Text>{value}</Text>
     </View>
   );
 };
 
+const colors = {
+  iconColor: brittishPalette.naval,
+};
+
 const styles = StyleSheet.create({
   wrapper: {
     width: '50%',
-    marginVertical: 5,
     padding: 10,
+    display: 'flex',
+  },
+  icon: {
+    alignSelf: 'flex-start',
   },
 });
 
